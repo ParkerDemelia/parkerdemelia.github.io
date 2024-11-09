62712b9b17d61b647f17b46c1a1db42e81e2def5
ParkerDemelia <parker.demelia@gmail.com>
2024-11-09 02:19:25 -0500

    website rework

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const logo = document.getElementById('logo');
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document loaded');
-});
+    logo.addEventListener('mouseover', () => {
+        let chars = "abcdefghijklmnopqrstuvwxyz1234567890/?!@#$%^&*";
+        for(let i = 0; i < 100; i++) {
+            let randomchar = chars[Math.floor(Math.random() * chars.length)];
+            logo.innerHTML += randomchar;
+        }
+    });
+}); 
